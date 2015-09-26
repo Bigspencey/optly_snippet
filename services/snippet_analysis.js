a9b3409baf6af56f0e303a870c04723c7a404118
Spencer Smitherman <spencer.smitherman@gmail.com>
2015-09-26 15:20:16 -0700

    Returning all portions of the snippet

--- services/snippet_analysis.js
+++ services/snippet_analysis.js
@@ -21,20 +21,29 @@ module.exports = function (req, callback) {
 			var stringifiedSnippet = _.trimRight(trimmedLeft, " }());");
 			var everythingAfterData = stringifiedSnippet.split('var DATA=')[1];
 			var experimentData = _.trimRight(everythingAfterData.split('\n')[0], ";");
+			var trimmedjQuery = stringifiedSnippet.split("var optimizelyCode")[0];
+			var finaljQuery = trimmedjQuery.split("optly.Cleanse.start();")[1].trim();
 			var parsedData = JSON.parse(experimentData);
+			callback(null, stringifiedSnippet, parsedData, finaljQuery);
+		},
+
+		function calculateLengths (stringifiedSnippet, parsedData, finaljQuery, callback) {
 			var goalExpressions = JSON.stringify(parsedData.goal_expressions).length + "goal_expressions".length;
 			var clickGoals = JSON.stringify(parsedData.click_goals).length + "click_goals".length;
 			var variationLength = JSON.stringify(parsedData.variations).length + "variations".length;
 			var sectionLength = JSON.stringify(parsedData.sections).length + "sections".length;
+			var totalSize = stringifiedSnippet.length;
 			// Lengths for each portion of snippet
-			var TotalSize = stringifiedSnippet.length;
+			var jQueryLength = finaljQuery.length;
 			var projectJSLength = parsedData.project_js.length + "project_js".length;
 			var audiencesLength = JSON.stringify(parsedData.audiences).length + "audiences".length;
 			var experimentLength = JSON.stringify(parsedData.experiments).length + "experiments".length;
 			var totalVaritionLength = variationLength + sectionLength;
 			var totalGoalsLength = goalExpressions + clickGoals;
-			console.log(parsedData);
-			callback(null, stringifiedSnippet);
+			var overHead = jQueryLength + projectJSLength + audiencesLength + experimentLength + totalVaritionLength + totalGoalsLength;
+			var optlyLength = totalSize - overHead;
+			var totals = [optlyLength, jQueryLength, projectJSLength, audiencesLength, experimentLength, totalVaritionLength, totalGoalsLength];
+			callback(null, totals);
 		}
 		],
 	function (err, results) {
