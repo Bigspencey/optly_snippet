5ae418bfe078ee6273170cf0d8321de5210c6474
Spencer Smitherman <spencer.smitherman@gmail.com>
2015-09-26 14:19:10 -0700

    Found snippet portion string lengths

--- services/snippet_analysis.js
+++ services/snippet_analysis.js
@@ -1,5 +1,6 @@
 var request = require('request');
 var async = require('async');
+var _ = require('lodash');
 
 module.exports = function (req, callback) {
 
@@ -11,13 +12,29 @@ module.exports = function (req, callback) {
 				if (!error && response.statusCode == 200){
 					snippet = body;
 				}
-			});
 			callback(null, snippet);
+			});
 		},
 
 		function sanitizeSnippet (snippet, callback) {
-			console.log(snippet);
-			callback(null, snippet);
+			var trimmedLeft = _.trimLeft(snippet.trim(), "(function(){ ");
+			var stringifiedSnippet = _.trimRight(trimmedLeft, " }());");
+			var everythingAfterData = stringifiedSnippet.split('var DATA=')[1];
+			var experimentData = _.trimRight(everythingAfterData.split('\n')[0], ";");
+			var parsedData = JSON.parse(experimentData);
+			var goalExpressions = JSON.stringify(parsedData.goal_expressions).length + "goal_expressions".length;
+			var clickGoals = JSON.stringify(parsedData.click_goals).length + "click_goals".length;
+			var variationLength = JSON.stringify(parsedData.variations).length + "variations".length;
+			var sectionLength = JSON.stringify(parsedData.sections).length + "sections".length;
+			// Lengths for each portion of snippet
+			var TotalSize = stringifiedSnippet.length;
+			var projectJSLength = parsedData.project_js.length + "project_js".length;
+			var audiencesLength = JSON.stringify(parsedData.audiences).length + "audiences".length;
+			var experimentLength = JSON.stringify(parsedData.experiments).length + "experiments".length;
+			var totalVaritionLength = variationLength + sectionLength;
+			var totalGoalsLength = goalExpressions + clickGoals;
+			console.log(parsedData);
+			callback(null, stringifiedSnippet);
 		}
 		],
 	function (err, results) {
