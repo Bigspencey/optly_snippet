23b53c2f7d309443f2c7a2db5c47e4666cc7afc4
Spencer Smitherman <spencer.smitherman@gmail.com>
2015-09-29 16:45:32 -0700

    removed closure removal function

--- services/snippet_analysis.js
+++ services/snippet_analysis.js
@@ -9,58 +9,46 @@ module.exports = function (project_id, callback) {
 			snippet;
 			request(url + project_id + ".js", function(error, response, body) {
 				if (!error && response.statusCode == 200){
-					snippet = body;
+					snippet = body.trim();
 				}
 			callback(null, snippet);
 			});
 		},
 
-		function removeClosure (snippet, callback) {
-			var trimmedLeft = snippet.trim().substr(13);
-			var stringifiedSnippet = trimmedLeft.slice(0, -7).trim();
-			callback(null, stringifiedSnippet);
+		function retrieveExperimentData (snippet, callback) {
+			var everythingAfterData = snippet.split('var DATA=')[1];
+			var experimentString = everythingAfterData.split('\n')[0].slice(0,-1);
+			var experimentData = JSON.parse(experimentString);
+			callback(null, snippet, experimentData);
 		},
 
-		function retrieveExperimentData (stringifiedSnippet, callback) {
-			var everythingAfterData = stringifiedSnippet.split('var DATA=')[1];
-			try {
-				var experimentString = everythingAfterData.split('\n')[0].slice(0,-1);
-				var experimentData = JSON.parse(experimentString);
-			}
-			catch (error) {
-				console.log(error);
-			}
-
-			callback(null, stringifiedSnippet, experimentData);
-		},
-
-		function retrievejQuery (stringifiedSnippet, experimentData, callback) {
-			var firstHalfOfSnippet = stringifiedSnippet.split("var optimizelyCode")[0];
+		function retrievejQuery (snippet, experimentData, callback) {
+			var firstHalfOfSnippet = snippet.split("var optimizelyCode")[0];
 			var jQuery = firstHalfOfSnippet.split("optly.Cleanse.start();")[1].trim();
-			callback(null, stringifiedSnippet, experimentData, jQuery)
+			callback(null, snippet, experimentData, jQuery)
 		},
 
-		function calculatejQuery (stringifiedSnippet, experimentData, jQuery, callback) {
+		function calculatejQuery (snippet, experimentData, jQuery, callback) {
 			var totals = {};
 			totals["jQuery"] = jQuery.length;
-			callback(null, stringifiedSnippet, experimentData, totals);
+			callback(null, snippet, experimentData, totals);
 		},
 
-		function calculateProjectJS (stringifiedSnippet, experimentData, totals, callback) {
+		function calculateProjectJS (snippet, experimentData, totals, callback) {
 			if (experimentData.project_js === undefined) {
 				totals["projectJS"] = 0;
 			} else {
 				totals["projectJS"] = experimentData.project_js.length + "project_js".length;
 			}
-			callback(null, stringifiedSnippet, experimentData, totals);
+			callback(null, snippet, experimentData, totals);
 		},
 
-		function calculateLengths (stringifiedSnippet, experimentData, finaljQuery, callback) {
+		function calculateLengths (snippet, experimentData, finaljQuery, callback) {
 			var goalExpressions = JSON.stringify(experimentData.goal_expressions).length + "goal_expressions".length;
 			var clickGoals = JSON.stringify(experimentData.click_goals).length + "click_goals".length;
 			var variationLength = JSON.stringify(experimentData.variations).length + "variations".length;
 			var sectionLength = JSON.stringify(experimentData.sections).length + "sections".length;
-			var totalSize = stringifiedSnippet.length;
+			var totalSize = snippet.length;
 			// Lengths for each portion of snippet
 			var jQueryLength = finaljQuery.length;
 			var audiencesLength = JSON.stringify(experimentData.audiences).length + "audiences".length;
